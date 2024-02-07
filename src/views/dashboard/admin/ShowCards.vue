ef974618d7a65bd829ef3deeb6bfc4c87b23b50e
Daniel Brenner <brenner@softwareprecisao.com.br>
2024-02-07 16:23:01 -0300

    Importando o sidebar no dashboard

--- src/views/dashboard/admin/ShowCards.vue
+++ src/views/dashboard/admin/ShowCards.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="wrapper">
+    <Sidebar>
+      <SidebarItem icon="lni-user" text="Profile" />
+      <SidebarItem icon="lni-agenda" text="Task" />
+      <!-- Adicione mais itens de menu aqui, se necessário -->
+    </Sidebar>
+
     <div class="main p-3">
+
+      <h1 class="text-center">teste de funcionamento</h1>
 
     </div>
   </div>
 </template>
 <script>
+import Sidebar from '../../../components/sidebar/Sidebar.vue'
+import SidebarItem from '../../../components/sidebar/SidebarItem.vue'
+
 export default {
-  name: 'ShowCards'
+  name: 'ShowCards',
+  components: {
+    Sidebar,
+    SidebarItem
+  }
 }
 </script>
